0ad2b768fbe481db82ad38b2e180ce65b0d394ed
Jan Hovland <jan.hovland@lyse.net>
2020-09-07 10:28:09 +0200

    Initial Commit

---
+++ MyTodoList/MyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoListApp.swift
+//  MyTodoList
+//
+//  Created by Jan Hovland on 07/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
